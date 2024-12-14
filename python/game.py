2570e3b5edce8d4fccfd45353e3de6b973469c7d
mbaunty1 <mrmbaunty@gmail.com>
2024-12-14 13:13:39 +0200

    Update game.py

--- python/game.py
+++ python/game.py
@@ -1,56 +1,112 @@
-import pygame
+from pygame import *
+from random import randint
 
-pygame.init()
+#
+mixer.init()
+mixer.music.load('space.ogg')
+mixer.music.play()
+fire_sound = mixer.Suund('fire.ogg')
 
-width = 800
-height = 600
+#
+font.init()
+font2 = font.Font(None, 36)
 
-window = pygame.display.set_mode((width, height))
-pygame.display.set_caption("Shooter")
 
-clock = pygame.time.Clock()
-game_over = False
+#
+img_back = "galaxy.jpg" 
+img_hero = "rocket"
+img_enemy = "ufo.png"
 
-background = pygame.transform.scale(pygame.image.load("galaxy.jpg"), (width, height))
+score = 0
+lost = 0 
 
-class GameSprite(pygame.sprite.Sprite):
-    def __init__(self, image, x, y, size, speed):
-        self.image = pygame.transform.scale(pygame.image.load(image), (size, size))
-        self.speed = speed
+#
+class GameSprite(sprite.Sprite):
+    #
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+        #
+        sprite.Sprite.__init__(self)
+
+        #
+        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.speed = player_speed
+
+        #
         self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
-    def draw(self):
+        self.rect.x = player_x
+        self.rect.y = player_y
+
+    #
+    def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
-
+#
 class Player(GameSprite):
+    #
     def update(self):
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_a]:
+        keys = key.get_pressed()
+        if keys[K_a]:
             self.rect.x -= self.speed
-        if keys[pygame.K_d]:
+        if keys[K_d]:
             self.rect.x =+ self.speed
-
+    
+    #
     def fire(self):
         pass
 
+#
+class Enemy(GameSprite):
+    #
+    def update(self):
+        self.rect.y += self.speed
+        global lost
+        #
+        if self.rect.y > win_height:
+            self.rect.x = randint(80, win_height - 80)
+            self.rect.y = 0
+            lost = lost + 1
+            
+#
+win_width = 700
+win_height = 500
+display.set_caption("Shooter")
+window = display.set_mode((win_width, win_height))
+background = transform.scale(image.load(img_back), (win_width, win_height))
 
-ship = Player("rocket.png", 400, 500, 80, 10)
+#
+ship = Player(img_hero, 5, win_height - 100, 80, 100, 10)
+
+#
+monster = sprite.Group()
+for i in range(1,6):
+    monster = Enemy(img_enemy, randint(80, win_width - 80), -40, 80, 50, randint(1, 5))
+    monster.add(monster)
 
 
+#
+finish = False
 
+#
+run = True
 
-while not game_over:
-    window.blit(background, (0, 0))
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            game_over = True
+while run:
+    #
+    for e in event.get():
+        if e.type == QUIT:
+            run = False
+    
+    if not finish:
+        #
+        window.blit(background, (0, 0))
 
-    ship.update()
-    ship.draw()
+        #
+        text = font2.render("Рахунок: " + str(score), 1, (255, 255, 255))
+        window.blit(text, (10, 20))
 
-    pygame.display.update()
-    clock.tick(60)
-
-pygame.quit()
+        #
+        ship.update()
+        monster.draw(window)
+        
+        display.update()
+    #
+    time.deplay(50)
